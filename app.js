61c8cf57ac622a69f49497162f40e361facdc7fb
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-07-31 19:20:12 -0300

    Fazendo toda a logica de funcionamento em JS

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomeAmigo = document.getElementById("amigo");
+    if(nomeAmigo === ""){
+        alert("Por favor, insira um nome.");
+    }
+    else{
+        amigos.push(nomeAmigo.value);
+        nomeAmigo.value = "";
+    }
+    atualizaListaDeAmigos();
+
+    let campoResultado = document.getElementById("resultado");
+    campoResultado.innerHTML = '';
+}
+
+function atualizaListaDeAmigos(){
+    limparLista();
+    for(let i = 0; i < amigos.length; i++){
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length > 0){
+        limparLista();
+        let indice = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = document.getElementById("resultado");
+        amigoSorteado.innerHTML = amigos[indice];
+    }
+    else {
+        alert("Insira amigos para sortear");
+    }
+}
+
+function limparLista(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+}
